b7e53d2fdd4367b419069d7001498c6c9d95d6b2
Alexey <finnlega@gmail.com>
2023-08-08 16:23:48 +0800

    Создает компоненты, для рендера на странице описание маршрута, стоимость, фильтры, сортировка, создание точек маршрута. Меняет config eslint. Подключает компонеты в файле main.js

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createMenuTemplate } from './view/menu';
+import { createTripInfoTemplate } from './view/trip-info';
+import { createCostTemplate } from './view/cost';
+import { createFilterTemplate } from './view/filters';
+import { createSortingTemplate } from './view/sorting';
+import { CreateFormAddNewPointTemplate } from './view/add-new-point';
+
+// Отрисовка компонента на странице
+
+const render = (container, component, place) => {
+  container.insertAdjacentHTML(place, component);
+};
+
+const tripMain = document.querySelector('.trip-main');
+const tripControls = tripMain.querySelector('.trip-controls__navigation');
+const tripFilters = tripMain.querySelector('.trip-controls__filters');
+const sorting = document.querySelector('.trip-events');
+
+render(tripMain, createTripInfoTemplate(), 'afterbegin');
+
+const tripInfo = tripMain.querySelector('.trip-main__trip-info');
+
+render(tripInfo, createCostTemplate(), 'beforeend');
+render(tripControls, createMenuTemplate(), 'beforeend');
+render(tripFilters, createFilterTemplate(), 'beforeend');
+render(sorting, createSortingTemplate(), 'beforeend');
+render(sorting, CreateFormAddNewPointTemplate(), 'beforeend');
+//

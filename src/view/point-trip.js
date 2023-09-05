d0dc72592b8ee678e953bfd34c00d6dfa4e20c33
Alexey <finnlega@gmail.com>
2023-09-05 17:11:34 +0800

    вычислит продолжительность поездки

--- src/view/point-trip.js
+++ src/view/point-trip.js
@@ -10,25 +10,69 @@ const getElement = (array) => {
   return offers.join('');
 };
 
+const convertDate = (date) => {
+  const timeFromDate = dayjs(date).format('HH:mm');
+  return timeFromDate;
+};
+
+const eventduration = (dateBegin, dateEnd) => {
+
+  // Преобразуем даты в объекты dayjs
+  const startDate = dayjs(dateBegin);
+  const endDate = dayjs(dateEnd);
+
+  // Рассчитываем разницу в минутах между датами
+  const minutesDiff = endDate.diff(startDate, 'minute');
+
+  // Разбиваем разницу на дни, часы и минуты
+  const days = Math.floor(minutesDiff / 1440);
+  const remainingMinutes = minutesDiff % 1440;
+  const hours = Math.floor(remainingMinutes / 60);
+  const minutes = remainingMinutes % 60;
+
+  // Форматируем результат в зависимости от условий
+  if (days === 0) {
+    if (hours === 0) {
+      if (minutes < 10) {
+        return `0${minutes}M`;
+      }
+      return `${minutes}M`;
+    } else {
+      const formattedHours = hours < 10 ? `0${hours}` : hours;
+      const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+      return `${formattedHours}H ${formattedMinutes}M`;
+    }
+  } else {
+    const formattedDays = days < 10 ? `0${days}` : days;
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    if (hours === 0 && minutes === 0) {
+      return `${formattedDays}D`;
+    } else {
+      return `${formattedDays}D ${formattedHours}H ${formattedMinutes}M`;
+    }
+  }
+};
+
 const createPointTripTemplate = (point) => {
-  // const element = 'done';
   // debugger;
-  const { basePrice, type, destination, offers } = point;
+  const { basePrice, type, destination, offers, dateBegin, dateEnd } = point;
   return (
     `<li class="trip-events__item">
       <div class="event">
-        <time class="event__date" datetime="2019-03-18"></time>
+        <time class="event__date" datetime="2019-03-18">${dayjs(dateEnd).format('MMM DD')}</time>
         <div class="event__type">
           <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
         </div>
         <h3 class="event__title">${type} ${destination.name}</h3>
         <div class="event__schedule">
           <p class="event__time">
-            <time class="event__start-time" datetime="2019-03-18T10:30"></time>
+            <time class="event__start-time" datetime="2019-03-18T10:30">${convertDate(dateBegin)}</time>
             &mdash;
-            <time class="event__end-time" datetime="2019-03-18T11:00"></time>
+            <time class="event__end-time" datetime="2019-03-18T11:00">${convertDate(dateEnd)}</time>
           </p>
-          <p class="event__duration">30M</p>
+          <p class="event__duration">${eventduration(dateBegin, dateEnd)}</p>
         </div>
         <p class="event__price">
           &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
